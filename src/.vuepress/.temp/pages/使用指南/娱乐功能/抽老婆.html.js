5f9fd0d2e844cd63666ede0dfbd9b9879f18c602
maimai993 <hjm184563@gmail.com>
2026-09-03 19:25:44 +0800

    1.11

---
+++ src/.vuepress/.temp/pages/使用指南/娱乐功能/抽老婆.html.js
@@ -0,0 +1,16 @@
+import comp from "D:/bot-docs/src/.vuepress/.temp/pages/使用指南/娱乐功能/抽老婆.html.vue"
+const data = JSON.parse("{\"path\":\"/%E4%BD%BF%E7%94%A8%E6%8C%87%E5%8D%97/%E5%A8%B1%E4%B9%90%E5%8A%9F%E8%83%BD/%E6%8A%BD%E8%80%81%E5%A9%86.html\",\"title\":\"抽老婆\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"抽老婆\",\"icon\":\"heart\",\"order\":29,\"description\":\"抽老婆 功能描述 抽取今日群友老婆，支持求婚、强娶、离婚等互动 使用方法 指令名称 参数说明 使用示例 娶群友 求婚 离婚 换老婆 查老婆\"},\"readingTime\":{\"minutes\":1.66,\"words\":497},\"filePathRelative\":\"使用指南/娱乐功能/抽老婆.md\",\"autoDesc\":true}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
